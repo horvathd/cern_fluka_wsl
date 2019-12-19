924ad0aef9e9cf13592e24d17af268f2c4ed111a
David Horvath <github@horvathd.eu>
2019-12-19 18:15:42 +0100

    Simplify with using the Flair repository

--- setup_wsl.sh
+++ setup_wsl.sh
@@ -1,77 +1,5 @@
 #!/bin/bash
 
-# Downloader function
-download_from_flair_website() {
-    # Download package
-    wget -q http://cern.ch/flair/download/$1
-
-    # Check if the download was successful
-    if [ "$?" -eq 0 ]; then
-        mv $1 ./downloads/
-    else
-        echo "[ERROR] Couldn't download $1. Please try again later."
-        exit 1
-    fi
-}
-
-install_flair() {
-    # Install flair from deb package
-    echo "Installing flair ${flair_latest}"
-    apt-get install -qq --reinstall --allow-downgrades ./downloads/$flair
-    if [ ! "$?" -eq 0 ]; then
-        echo "[ERROR] Couldn't install $flair. Please conctact the Fluka forum for help."
-        exit 1
-    fi
-
-    echo "Installing flair-geoviewer ${flair_latest}"
-    mkdir -p ./tmp-geoviewer
-
-    # Compile and install flair-geoviewer from source
-    tar -zxf ./downloads/$geoviewer -C ./tmp-geoviewer
-    cd ./tmp-geoviewer/flair-geoviewer-*
-
-    make --silent -j4
-    if [ ! "$?" -eq 0 ]; then
-        echo "[ERROR] Couldn't install $geoviewer. Please conctact the Fluka forum for help."
-        exit 1
-    fi
-
-    make --silent install
-    if [ ! "$?" -eq 0 ]; then
-        echo "[ERROR] Couldn't install $geoviewer. Please conctact the Fluka forum for help."
-        exit 1
-    fi
-
-    cd ../..
-    rm -rf ./tmp-geoviewer
-}
-
-force_reinstall_flag=false
-flair_requested_flag=false
-
-# Check command line arguments
-while true; do
-    case $1 in
-        --force_reinstall)
-            force_reinstall_flag=true
-            ;;
-        --flair)
-            flair_requested_flag=true
-            flair_requested="$2"
-            shift
-            ;;
-        -h | --help)
-            echo
-            echo "Usage: sudo ./setup_wsl [--flair <version_number>]"
-            echo "                        [--force_reinstall]"
-            echo "                        [-h / --help]"
-            echo
-            exit 0
-            ;;
-    esac
-    shift || break
-done
-
 # Check if the script run as root
 if [ "$EUID" -ne 0 ]; then
     echo "[ERROR] The installation script requires root privileges"
@@ -79,14 +7,27 @@ if [ "$EUID" -ne 0 ]; then
     exit 1
 fi
 
-if [ "$force_reinstall_flag" = true ]; then
-    echo "[OPTION] Forced reinstall enabled"
-fi
+# Add Flair repository
+REPO="https://cern.ch/flair/download/ubuntu/18.04"
 
-if [ "$flair_requested_flag" = true ]; then
-    echo "[OPTION] Custom Flair version (${flair_requested}) requested"
-fi
+# If repository is not present
+if ! grep -q "$REPO" /etc/apt/sources.list; then
+    echo "Adding Flair repository"
 
+    # Add GPG key
+    wget -q -O - https://cern.ch/flair/download/ubuntu/KEY.gpg | sudo apt-key add -
+    if [ ! "$?" -eq 0 ]; then
+        echo "[ERROR] Couldn't add repository GPG key. Try again later."
+        exit 1
+    fi
+
+    # Add repository
+    add-apt-repository "deb [arch=all,amd64] $REPO  ./"
+    if [ ! "$?" -eq 0 ]; then
+        echo "[ERROR] Couldn't add repository. Try again later."
+        exit 1
+    fi
+fi
 
 # Install required packages
 echo "Updating package list"
@@ -99,9 +40,16 @@ fi
 echo "Installing necessary packages"
 apt-get install -y -qq make gawk gfortran gfortran-8 tk gnuplot-x11 \
     python3 python3-tk python3-pil python3-pil.imagetk python3-numpy \
-    python3-scipy python3-dicom ed g++ tk-dev python3-dev
+    python3-scipy python3-dicom
 if [ ! "$?" -eq 0 ]; then
     echo "[ERROR] Couldn't install the necessary packages. Try again later."
+    exit 1
+fi
+
+echo "Installing Flair"
+apt-get install -y -qq flair
+if [ ! "$?" -eq 0 ]; then
+    echo "[ERROR] Couldn't install Flair. Try again later."
     exit 1
 fi
 
@@ -125,86 +73,5 @@ if ! grep --quiet "PATH=\$PATH:/usr/local/fluka/bin/" ~/.bashrc; then
     echo "export DISPLAY=:0" >> ~/.bashrc
     echo "export XMING=True" >> ~/.bashrc
 fi
-
-# Create downloads folder if doesn't exists
-mkdir -p ./downloads
-
-# Get Flair version to install
-if [ "$flair_requested_flag" = true ]; then
-    flair_latest=$flair_requested
-else
-    # Check latest flair version from the flair website
-    echo "Checking flair version"
-    wget -q http://cern.ch/flair/download/version.tag
-
-    if [ "$?" -eq 0 ]; then
-        prefix=flair-
-        suffix=.tgz
-
-        flair_latest=$(cat version.tag | awk '{print $1}' | sed -e "s/^$prefix//" -e "s/$suffix$//")
-
-        \rm -rf version.tag
-    else
-        flair_latest=0
-    fi
-fi
-
-# Create directory for storing installed version numbers
-mkdir -p ~/.fluka
-
-# File storing the installed version number
-flair_version_file=~/.fluka/flair.version
-
-# Check installed Flair version
-if [ "$force_reinstall_flag" = true ]; then
-    flair_installed=0
-elif [ -f "${flair_version_file}" ]; then
-    flair_installed=$(cat ${flair_version_file})
-
-    # If latest flair version is not available, assume installed one is up to date
-    if [ "${flair_latest}" == "0" ]; then
-        flair_latest=flair_installed
-        echo "[WARNING] Couldn't determine latest Flair version, assuming installed version is up to date"
-    fi
-else
-    flair_installed=0
-fi
-
-# INstall flair
-if [ "${flair_latest}" == "0" ]; then
-    echo "[WARNING] Couldn't determine latest Flair version, trying to install latest"
-
-    flair=flair_latest_all.deb
-    geoviewer=flair-geoviewer_latest.tgz
-
-    # Download latest version of flair and flair-geoviewer
-    download_from_flair_website $flair
-    download_from_flair_website $geoviewer
-
-    install_flair
-elif [ ! "${flair_latest}" == "${flair_installed}" ]; then
-    flair=flair_${flair_latest}_all.deb
-    geoviewer=flair-geoviewer-${flair_latest}.tgz
-
-    if [ "$force_reinstall_flag" = true ]; then
-        \rm -rf  "./downloads/$flair"
-        \rm -rf "./downloads/$geoviewer"
-    fi
-
-    if [ ! -f "./downloads/$flair" ]; then
-        download_from_flair_website $flair
-    fi
-
-    if [ ! -f "./downloads/$geoviewer" ]; then
-        download_from_flair_website $geoviewer
-    fi
-
-    install_flair
-else
-    echo "Flair (${flair_installed}) is up to date"
-fi
-
-touch "${flair_version_file}"
-echo "${flair_latest}" > "${flair_version_file}"
 
 echo "Install complete"
