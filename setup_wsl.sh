611e88d4335058909cd619cfc6369346c5a64be2
David Horvath <github@horvathd.eu>
2020-04-23 20:01:14 +0200

    Reorganize script, remove package installation with pip

--- setup_wsl.sh
+++ setup_wsl.sh
@@ -13,15 +13,31 @@ ubuntu_codename=$(lsb_release -c | awk '{print $2}')
 # Set Flair repository
 if [ "$ubuntu_codename" = "bionic" ]; then
     REPO="https://cern.ch/flair/download/ubuntu/18.04"
-    gfortran="gfortran-7"
 elif [ "$ubuntu_codename" = "focal" ]; then
     REPO="https://cern.ch/flair/download/ubuntu/20.04"
-    gfortran="gfortran-9"
 
     echo "[ERROR] The support for Ubuntu 20.04 is not yet implemented"
     exit 1
 else
     echo "[ERROR] The installation script requires Ubuntu 18.04 or 20.04"
+    exit 1
+fi
+
+# Packages to install
+packages="make gawk gfortran tk gnuplot-x11 python3 python3-tk python3-pil python3-pil.imagetk python3-numpy python3-scipy python3-dicom"
+
+# Install required packages
+echo "Updating package list"
+apt-get update -qq
+if [ ! "$?" -eq 0 ]; then
+    echo "[ERROR] Couldn't update pacakge list. Try again later."
+    exit 1
+fi
+
+echo "Installing necessary packages"
+apt-get install -y -qq $packages
+if [ ! "$?" -eq 0 ]; then
+    echo "[ERROR] Couldn't install the necessary packages. Try again later."
     exit 1
 fi
 
@@ -43,36 +59,6 @@ if ! grep -q "$REPO" /etc/apt/sources.list; then
         exit 1
     fi
 fi
-
-# Install required packages
-echo "Updating package list"
-apt-get update -qq
-if [ ! "$?" -eq 0 ]; then
-    echo "[ERROR] Couldn't update pacakge list. Try again later."
-    exit 1
-fi
-
-# Packages to install
-packages="make gawk gfortran tk gnuplot-x11 python3 python3-pip python3-tk python3-pil python3-pil.imagetk python3-numpy python3-scipy"
-
-echo "Installing necessary packages"
-apt-get install -y -qq $packages
-if [ ! "$?" -eq 0 ]; then
-    echo "[ERROR] Couldn't install the necessary packages. Try again later."
-    exit 1
-fi
-
-# Install pydicom
-pip3 --no-cache-dir install pydicom
-if [ ! "$?" -eq 0 ]; then
-    echo "[ERROR] Couldn't install the necessary packages. Try again later."
-    exit 1
-fi
-
-# Set gfortran-7 as default
-echo "Setting up ${gfortran} as default compiler"
-update-alternatives --quiet --remove-all gfortran
-update-alternatives --quiet --install /usr/bin/gfortran gfortran /usr/bin/${gfortran} 10
 
 echo "Installing Flair"
 apt-get install -y -qq flair
