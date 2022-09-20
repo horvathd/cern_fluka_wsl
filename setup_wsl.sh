e474f8680fcccf3919b19b4d8a47beda262efc1c
David Horvath <github@horvathd.eu>
2022-09-20 13:26:38 +0200

    Add support for the HTML manual (#1)
    Reorganize code and add Firefox to the package list

--- setup_wsl.sh
+++ setup_wsl.sh
@@ -7,21 +7,46 @@ if [ "$EUID" -ne 0 ]; then
     exit 1
 fi
 
+# Trying to determine WSL version
+uname=$(uname -r)
+
+if [ $(echo ${uname} | grep "Microsoft") ]; then
+    WSL_version="1"
+    echo "WSL version 1 has been detected"
+elif [ $(echo ${uname} | grep "microsoft-standard") ]; then
+    WSL_version="2"
+    echo "WSL version 2 has been detected"
+else
+    WSL_version="0"
+    echo "[WARNING] WSL version could not be determined. Assuming native Linux installation."
+fi
+
 # Check Ubuntu version
 ubuntu_codename=$(lsb_release -c | awk '{print $2}')
 
-# Set Flair repository
+# Set repositories
 if [ "$ubuntu_codename" = "focal" ]; then
     REPO="https://cern.ch/flair/download/ubuntu/20.04"
 elif [ "$ubuntu_codename" = "jammy" ]; then
     REPO="https://cern.ch/flair/download/ubuntu/22.04"
+
+    if [ $WSL_version != "0" ]; then
+        sudo add-apt-repository ppa:mozillateam/ppa
+
+        echo 'Package: *
+Pin: release o=LP-PPA-mozillateam
+Pin-Priority: 1001
+' | tee /etc/apt/preferences.d/mozilla-firefox
+
+        echo 'Unattended-Upgrade::Allowed-Origins:: "LP-PPA-mozillateam:${distro_codename}";' | tee /etc/apt/apt.conf.d/51unattended-upgrades-firefox
+    fi
 else
     echo "[ERROR] The installation script requires Ubuntu 20.04 or 22.04"
     exit 1
 fi
 
 # Packages to install
-packages="make gawk gfortran tk gnuplot-x11 python3 python3-tk python3-pil python3-pil.imagetk python3-numpy python3-scipy python3-matplotlib python3-dicom ttf-ancient-fonts"
+packages="make gawk gfortran tk gnuplot-x11 python3 python3-tk python3-pil python3-pil.imagetk python3-numpy python3-scipy python3-matplotlib python3-dicom ttf-ancient-fonts firefox"
 
 # Install required packages
 echo "Updating package list ..."
@@ -43,7 +68,7 @@ if ! grep -q "$REPO" /etc/apt/sources.list; then
     echo "Adding Flair repository ..."
 
     # Add GPG key
-    wget -q -O - https://cern.ch/flair/download/ubuntu/flair.gpg | apt-key add -
+    sudo wget -q -O /etc/apt/trusted.gpg.d/flair.asc https://cern.ch/flair/download/ubuntu/flair.gpg
     if [ ! "$?" -eq 0 ]; then
         echo "[ERROR] Couldn't add repository GPG key. Try again later."
         exit 1
@@ -65,33 +90,13 @@ if [ ! "$?" -eq 0 ]; then
 fi
 
 # Set up necessary envionment variables
-echo "Setting up DISPLAY environmental variable ..."
 scriptname="/etc/profile.d/flair_wsl.sh"
 
-# Trying to determine WSL version
-uname=$(uname -r)
-
-if [ $(echo ${uname} | grep "Microsoft") ]; then
-    WSL_version="1"
-    echo "WSL version 1 has been detected"
-elif [ $(echo ${uname} | grep "microsoft-standard") ]; then
-    WSL_version="2"
-    echo "WSL version 2 has been detected"
-else
-    echo "[WARNING] WSL version could not be determined."
-    echo "Please set WSL version manually (1 or 2):"
-    read WSL_version
-
-    if [ $WSL_version != "1" ] && [ $WSL_version != "2" ]; then
-        echo "[ERROR] Unsupported WSL version."
-        exit 1
-    fi
-fi
-
-# Writing script for DISPLAY
 if [ $WSL_version == "1" ]; then
+    echo "Setting up DISPLAY environmental variable ..."
     echo "export DISPLAY=:0" > $scriptname
 elif [ $WSL_version == "2" ]; then
+    echo "Setting up DISPLAY environmental variable ..."
     echo "export DISPLAY=\`\grep nameserver /etc/resolv.conf | sed 's/nameserver //'\`:0" > $scriptname
 fi
 
