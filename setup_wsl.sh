e03d8b98f70d87d6edc60fa97e9a28d27c8770db
David Horvath <github@horvathd.eu>
2026-02-20 09:21:11 +0100

    Update Ubuntu version check in setup_wsl.sh

--- setup_wsl.sh
+++ setup_wsl.sh
@@ -26,9 +26,7 @@ fi
 ubuntu_codename=$(lsb_release -sc 2> /dev/null)
 
 # Set repositories
-if [ "$ubuntu_codename" = "focal" ]; then
-    : # Pass
-elif [ "$ubuntu_codename" = "jammy" ] || [ "$ubuntu_codename" = "noble" ]; then
+if [ "$ubuntu_codename" = "jammy" ] || [ "$ubuntu_codename" = "noble" ]; then
     if [ $WSL != "0" ]; then
         # Adding Firefox repository for WSL 2, instead of the snapd version
         echo " - Adding Firefox repository"
@@ -43,7 +41,7 @@ Pin-Priority: 1001
         | tee /etc/apt/apt.conf.d/51unattended-upgrades-firefox >> $logfile 2>&1
     fi
 else
-    echo "   [ERROR] The installation script requires Ubuntu 20.04, 22.04 or 24.04"
+    echo "   [ERROR] The installation script requires Ubuntu 22.04 or 24.04"
     exit 1
 fi
 
